3bf3791c0638f70033b006130a6a1347c6a64eef
Athif Binu <binuathif@gmail.com>
2026-03-27 16:34:33 +0530

    deplyment problem

--- app/superadmin/page.tsx
+++ app/superadmin/page.tsx
@@ -1,3 +1,3 @@
 export default function SuperAdminPage() {
-  return <h1 className="text-xl font-bold">Welcome to Super Admin Panel 👑</h1>;
+  return <h1 className="text-xl font-bold">Welcome to  Super Admin Panel 👑</h1>;
 }
